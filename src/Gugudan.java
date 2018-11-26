21fd37aad0582c597df79dadfe9fcb795b0420a7
dongyeup <dongyeup@DESKTOP-K4HKUOE>
2018-11-26 18:19:54 +0900

    2

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class Gugudan {
 
+	public static void main(String[] args) {
+		int num; // 출력할 구구단 수	
+		Scanner scanner = new Scanner(System.in);
+		num = scanner.nextInt();
+		System.out.println("구구단 중 출력할 단을 입력하시오 : "+num);
+		
+		for(int i=1;i<10;i++)
+		{
+			System.out.println(num*i);
+		}
+	}
 }
