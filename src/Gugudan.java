3c906774459ca4939dde2173b75550a9d5248e34
dongyeup <dongyeup@DESKTOP-K4HKUOE>
2018-11-28 10:35:31 +0900

    반복

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -4,13 +4,23 @@ public class Gugudan {
 
 	public static void main(String[] args) {
 		int num; // 출력할 구구단 수	
+		System.out.println("구구단 중 출력할 단을 입력하시오 : ");
 		Scanner scanner = new Scanner(System.in);
-		num = scanner.nextInt();
-		System.out.println("구구단 중 출력할 단을 입력하시오 : "+num);
+		num = scanner.nextInt();		
+		System.out.println(num);
 		
-		for(int i=1;i<10;i++)
+		/*for(int i=1;i<10;i++)
 		{
 			System.out.println(num*i);
+		}*/
+		
+		int i=1;
+		while(i<10)
+		{
+			System.out.println(num*i);
+			i++;
 		}
+		
+		
 	}
 }
