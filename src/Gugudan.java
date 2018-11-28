c3bbb070c213f85d6eb9eefcf5821ca31c6e28ce
dongyeup <dongyeup@DESKTOP-K4HKUOE>
2018-11-28 10:51:40 +0900

    수정

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -4,20 +4,28 @@ public class Gugudan {
 
 	public static void main(String[] args) {
 		int num; // 출력할 구구단 수	
-		System.out.println("구구단 중 출력할 단을 입력하시오 : ");
 		Scanner scanner = new Scanner(System.in);
 		num = scanner.nextInt();		
-		System.out.println(num);
+		System.out.println("구구단 중 출력할 단을 입력하시오 : "+num);
 		
 		/*for(int i=1;i<10;i++)
 		{
 			System.out.println(num*i);
 		}*/		
-		int i=1;
+		/*int i=1;
 		while(i<10)
 		{
 			System.out.println(num*i);
 			i=i+1;
+		}*/
+		if(num>1 && num<10)
+		{
+			for(int i=1;i<10;i++)
+			{
+				System.out.println(num*i);
+			}
+		}
+		else
+			System.out.println("잘못입력했다.");
 		}		
-	}
 }
